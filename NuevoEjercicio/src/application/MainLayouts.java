a0992e9704c8f5a5b8d072fe87bb90c0f5449077
mariminnie <mariaolmo.instituto@gmail.com>
2021-03-16 20:07:54 +0100

    Cambio del Main

--- NuevoEjercicio/src/application/MainLayouts.java
+++ NuevoEjercicio/src/application/MainLayouts.java
@@ -1,24 +1,31 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class MainLayouts extends Application {
+	private BorderPane rootLayout;
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	 try {
+	 // Carga el diseño del archivo FXML en la variable rootLayout
+	 FXMLLoader loader = new FXMLLoader();
+	 loader.setLocation(MainLayouts.class.getResource("MainLayouts.fxml"));
+	 rootLayout = (BorderPane) loader.load();
+
+	 // Mostramos la escena del BorderPane de la variable rootLayot
+	 Scene scene = new Scene(rootLayout);
+	 primaryStage.setScene(scene);
+	 primaryStage.show();
+	 } catch(Exception e) {
+	e.printStackTrace();
+	 }
 	}
+
 	
 	public static void main(String[] args) {
 		launch(args);
